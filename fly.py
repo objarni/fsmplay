6070565f1fddf6ac2c12121ca76e1d7ff1ba9b49
Olof Bjarnason <olof.bjarnason@gmail.com>
2015-02-17 22:42:58 +0100

    removed deprecated comments

--- fly.py
+++ fly.py
@@ -7,43 +7,26 @@ the room. When it is not wandering around on human skin, it
 flies around in the room, looking for a place to sit and rest
 awhile, and then it starts looking for human skin again.
 
-So it has these different states:
-1 Flying / Looking for restplace
-2 Flying / Looking for skin
-3 Sitting / Resting
-4 Walking / Looking for food on skin
-5 Sitting / Eating on skin
-6 Dead / Hit by human or starvation
-
 To make things simple, the only property the fly has is it's "energy level".
-When this is low, the fly look for food, or die if not replenished.
+When this is low, the fly look for food.
 When it is high, the fly will try and rest.
 
-The energy level is reduced by flying (a lot) and walking (a little), and
-minimally by resting sitting still. It is replenished by sitting and eating.
+When the fly is on human skin, there is a chance it gets hit by the human.
 
-When the fly is on human skin, there is a chance it gets hit by the human,
-or else it flies away and starts looking for skin again.
-
-If it does not replenish energy level before it reaching zero, it dies.
-
-There is only so much food on the human skin, so eventually the fly dies
-of starvation (if not dead before by being hit!).
-
-This means the state visible to each state is 'energy' and 'food'.
-Energy is internal to the fly, and Food is the world food.
-
-(also, a random number generator will determine random events, so it's
-kind of part of the state too, but let's ignore that).
-
-*** Simplification of states ***
-In a simpler model, we ignore the two "flying" states, and see them
-as transitions instead. We get:
+Three clear states emerge:
 
 1 Resting
-2 Searching (flying or walking)
-3 Eating
-4 Dead
+2 Eating
+3 Dead
+
+Possible extensions:
+- the food is never exhausted in current model. it could be finite,
+  thus letting the fly die of starvation.
+- currently there is no state "moving" where the fly flies or walks on skin.
+  during such times, the energy level should lessen quicker than in resting
+  state as it's a lot more energy consuming activities!
+- what about if the human wakes up? then (s)he could smite the fly with
+  much higher probability, and in more states than "EATING". a second FSM to model this?
 '''
 
 import unittest
@@ -52,43 +35,6 @@ from statemachine import StateMachine
 import logging
 logging.basicConfig(level=logging.DEBUG)
 import random
-
-'''
-def flying_to_rest(cargo):
-    cargo.energy -= 1
-    if cargo.energy == 0:
-        return ('dead', cargo)
-    else:
-        print "I'm flying for a nap now."
-        return ('flying_to_skin', cargo)
-
-def flying_to_skin(cargo):
-    print "I'm flying to skin."
-    if random.uniform(0, 100) > 90:
-        print "Scheize, got wacked!"
-        return ('dead', cargo)
-    else:
-        return ('flying_to_rest', cargo)
-
-def dead(cargo):
-    pass  # End state is never executed
-'''
-
-'''
-def exploring_skin():
-    pass
-
-def resting():
-    print "I'm resting on the wall."
-
-def eating_on_skin():
-    print "Wo-hoo, eating food!"
-
-
-def dead():
-    print 'Bummer, died.'
-    return 'dead'
-'''
 
 
 class Cargo(object):
